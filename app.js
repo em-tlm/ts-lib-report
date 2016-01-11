e5be5be5d8aa8db3d3caedc2dad1eb0d1badcee3
emeryotopalik <emerycgo@gmail.com>
2016-01-11 11:11:51 -0500

    add doublejank.js

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
+//function server() {
+
+console.log("here");
+
 var express = require('express');
 var app = express();
-//var path = require('path');
+var path = require('path');
 
-//var toPdf = require('./public/htmlToPdf')
 var toPdf = require(path.join(__dirname, 'public/htmlToPdf.js'));
+
 console.log('Server is up');
 
 app.use(express.static('public'));
 
 app.get('/createPDF', function(req, res) {
-	res.send(pdf);
-	//res.download(toPdf.output());
+	//res.send(toPdf.output());
+	res.download(toPdf.output());
+	res.send("success!");
 });
 
 app.listen(process.env.PORT || 5555);
+
+//};
