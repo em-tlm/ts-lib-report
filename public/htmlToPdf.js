1a44a71db109e8e0e1a563a66928f9e2dd9a84ae
emeryotopalik <emerycgo@gmail.com>
2016-01-11 12:09:19 -0500

    jank.html=generation.html, doublejank.html=plots_generatePDF.html

--- public/htmlToPdf.js
+++ public/htmlToPdf.js
@@ -1,8 +1,8 @@
 exports.output = function() {
 
 var phantom = require('phantom');
-var html = 'http://localhost:5555/jank.html';
-var file = 'public/output/jank.pdf';
+var html = 'http://localhost:5555/generation.html';
+var file = 'public/output/plots.pdf';
 
 
   phantom.create(function(ph){
@@ -23,50 +23,3 @@ var file = 'public/output/jank.pdf';
 });
   return file;
 };
-
-
-// MAKE MULTIPLE PAGES INTO PDFs
-// var urls = ["http://localhost:5555/table.html", "http://localhost:5555/charts.html"];
-// var files = ["output/table.pdf", "output/charts.pdf"];
-
-// phantom.create(function(ph){
-// ph.createPage(function(page) {
-//   page.viewportSize = { width: 1920, height: 1920 };   
-//   page.set('paperSize', {format: 'Letter', orientation: 'portrait'});
-//   page.set('zoomFactor', 1);
-//   function render(urls, files, callback) {
-//     if (urls.length == 0) {
-//       console.log("Exiting...");
-//       ph.exit();
-//       if (callback) callback();
-//         return;
-//     }
-//     var url = urls[0];
-//     var file = files[0];
-//     page.open(url, function(){
-//       page.render(file);
-//       render(urls.slice(1), files.slice(1), callback);
-
-//     });
-//     }
-//     render(urls, files);
-//   });
-// });
-
-
-
-/*var fs = require('fs');
-var pdf = require('html-pdf');
-var html = fs.readFileSync('logsTable.html', 'utf8');
-//var html = "<h1>HI<\h1>";
-var options = {  type: 'png', orientation: 'portrait', format: 'letter' };
-
-pdf.create(html, options).toFile('test.png', function(err, res) {
-  if (err) return console.log(err);
-  console.log(res); // { filename: '/app/businesscard.pdf' }
-});*/
-
-/*
-var pdfMake = require('pdfmake');
-var docDefinition = {content: 'THIS IS CONTENT'};
-pdfMake.createPdf(docDefinition).open();*/
