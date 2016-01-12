0d44a56d8facd1cf35fe9e3e26b027f3c10122e9
emeryotopalik <emerycgo@gmail.com>
2016-01-12 17:06:17 -0500

    EJS RENDER

--- public/htmlToPdf.js
+++ public/htmlToPdf.js
@@ -2,28 +2,77 @@
 // call pdf.output("path/to/output/file", data, 
 // 			{timestamp: "on", pageNumbers: "on", type: "table", "pie", "graph", or 'provide a template'})
 
-exports.output = function(out, data, options) {
 
+// for array of one chart type: generate an html page with looping of array data and frames of data type
+// might not work for tables.... but could do several pie charts and graphs
+// could do array of types???
+
+var rawdata = 0;
+
+getData = function() {
+  return [{Timestamp:"12:00:03",TSLink:"TS-Link0",Username:"user-name0",Event:"event-0"}];
+  //return rawdata;
+};
+
+exports.output = function(out, data, options) {
+  //rawdata = data;
 	// options: timestamp, header with page numbers, own html template
 	// {timestamp: "on", pageNumbers: "on", type: ("table", "pie", "graph", or 'provide a template')}
 	var path = require('path');
+  var ejs = require('ejs');
+  var phantom = require('phantom');
+  var fs = require('fs');
+
 
 	if (options === undefined) options = {};
 
   var path = path.join(__dirname, 'templates/');
 
+  // var keys = Object.keys(data[0]);
+  // var keyOutput = ;
+  // for (var i = 0; i < keys.length; i++) {
+
+  // }
+
 	var html;
 	if ("type" in options) {
-		if (options.type === "pie") { html = 'file://' + path + 'usageReportPie.html'; }
-		else if (options.type === "table") { html = 'file://' + path + 'logsTable.html'; }
-		else if (options.type === "graph") { html = 'file://' + path + 'timeSeriesDataPlot.html'; }
+		if (options.type === "pie") { html = 'file://' + path + 'usageReportPieTemplate';}//?data=' + JSON.stringify(data); }
+		else if (options.type === "table") { html = 'file://' + path + 'logsTableTemplate';}//?data=1,2,3'; } //+ JSON.stringify(data); }
+		else if (options.type === "graph") { html = 'file://' + path + 'timeSeriesDataPlotTe
+    mplate';}//?data=' + JSON.stringify(data); }
  		else html = options.type;
  	} else {
  		console.log("ERROR: 'type' undefined");
  		return;
  	}
 
-var phantom = require('phantom');
+// ejs.compile(data);
+// var htmlRender = ejs.render(html, {raw : 'wow'} );
+// console.log(htmlRender);
+function ejs2html(path, information) {
+    fs.readFile(path, 'utf8', function (err, data) {
+        if (err) { console.log(err); return false; }
+        var ejs_string = data,
+            template = ejs.compile(ejs_string),
+            html = template(information);
+        fs.writeFile(path + '.html', html, function(err) {
+            if(err) { console.log(err); return false }
+            return true;
+        });  
+    });
+}
+
+
+
+ejs2html(__dirname+"/templates/usageReportPieTemplate.ejs",
+  {
+  pagename: 'Pie',
+   raw: JSON.stringify(data)
+  });
+
+var htmlRendered = __dirname+"/templates/usageReportPieTemplate.ejs.html";
+
+
   phantom.create(function(ph){
   ph.createPage(function(page) {
     page.viewportSize = { width: 1920, height: 1920 };
@@ -61,7 +110,12 @@ var phantom = require('phantom');
 		page.set('paperSize', {format: 'Letter', orientation: 'portrait', margin:'1cm'});
 	}
     page.set('zoomFactor', 1);
-    page.open(html, function(status) {
+    // page.set('content',htmlRender);
+    // page.render(out, function() {
+    //   console.log('Page Rendered');
+    //   ph.exit();
+    // });
+    page.open(htmlRendered, function(status) {
     	if (status === "success") {
     		console.log("Page Opened");
     		page.render(out,function(){
@@ -75,4 +129,5 @@ var phantom = require('phantom');
   });
 });
 };
+
 
--- public/test.js
+++ public/test.js
@@ -2,7 +2,11 @@ var pdf = require('./htmlToPdf.js');
 var args = process.argv.slice(1);
 if (args.length < 3) {
 	//console.log("Insufficient Arguments:\n\n\tnode test.js 'path/to/output/file' 'type' data");
-	pdf.output("output/table.pdf", 3, {type: "table", pageNumbers: "off", timestamp: "on"});
+	pdf.output("output/pie.pdf", 
+			[{label:"Running", data:20},
+               {label:"Idle", data:60},
+               {label:"Stalled", data:20}], 
+               {type: "pie", pageNumbers: "off", timestamp: "on"});
 } else {
 	pdf.output(args[1], args[2], args[3]);
 }
