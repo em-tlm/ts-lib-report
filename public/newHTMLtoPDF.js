dc87d6a1f26aa5fe1be47212ae4c7338bf0959fd
Wendy Wei <wendywei@mit.edu>
2016-01-12 13:40:38 -0500

    template module

--- public/newHTMLtoPDF.js
+++ public/newHTMLtoPDF.js
@@ -1,38 +1,28 @@
-// call exports.output(generateHTML(type, data), output));
-
-exports.generateHTML = function(type, data) { // type = pie chart, table, plotted graph
- 	if (type === "pie") { html = 'http://localhost:5555/usageReportPie.html'}
-	else if (type === "table") { html = 'http://localhost:5555/logsTable.html'}
-	else if (type === "graph") { html = 'http://localhost:5555/timeSeriesDataPlot.html'}
- 	else { html = "ERROR" }
-};
-
 exports.output = function(output, type, data) {
-var html;
-	if (type === "pie") { html = 'http://localhost:5555/usageReportPie.html'}
-	else if (type === "table") { html = 'http://localhost:5555/logsTable.html'}
-	else if (type === "graph") { html = 'http://localhost:5555/timeSeriesDataPlot.html'}
- 	else { html = "ERROR" }
+var template;
+	if (type === "pie") { template = 'http://localhost:5555/pie'}
+	else if (type === "table") { template = 'http://localhost:5555/logsTable.html'}
+	else if (type === "graph") { template = 'http://localhost:5555/timeSeriesDataPlot.html'}
+ 	else { template = "ERROR" }
+
+var phantom = require('phantom'),
+    ejs = require('ejs');
+
+var html = ejs.render(template, {
+    pagename: 'wow'
+});
 
 
-var phantom = require('phantom');
-//var html = 'http://localhost:5555/generation.html'; // input
-//var file = 'public/output/plots.pdf'; // output
-
-
-  phantom.create(function(ph){
+phantom.create(function(ph){
   ph.createPage(function(page) {
     page.viewportSize = { width: 1920, height: 1920 };   
     page.set('paperSize', {format: 'Letter', orientation: 'portrait', margin:'1cm'});
     page.set('zoomFactor', 1);
 
-    page.open(html, function(status) {
-      console.log(status);
-    
-        page.render(output,function(){
-        console.log('Page Rendered');
-        ph.exit();
-      });
+    page.set('content', html);
+    page.render('test.pdf',function(){
+    console.log('Page Rendered');
+    ph.exit();
     });
   });
 });
