9678b7973cc4985402784f45a142c07ff5ef963d
Wendy Wei <wendywei@mit.edu>
2016-01-13 09:38:39 -0500

    cleanup

--- public/version copy.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var phantom = require('phantom');
-console.log('using PhantomJS version ' +
-  phantom.version.major + '.' +
-  phantom.version.minor + '.' +
-  phantom.version.patch);
-phantom.exit();
