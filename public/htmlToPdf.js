96409dc8b242ab1a4f9053b113139fbe7eda60fb
emeryotopalik <emerycgo@gmail.com>
2016-01-20 15:13:15 -0500

    array, template building from data, still takes personal templates

--- public/htmlToPdf.js
+++ public/htmlToPdf.js
@@ -1,129 +1,114 @@
-var PDFs = [];
+var ejs = require('ejs');
+var fs = require('fs');
+var path = require('path');
+var phantom = require('phantom');
+var Q = require('q'); 
+var exec = require('child_process').exec;
+var curpath = path.join(__dirname, 'templates/'); 
 
-exports.concat = function(output) {
-  // how to deal with page numbers?
-  var fs = require('fs');
-  var exec = require('child_process').exec;
-  var PDFMerge = require('pdf-merge');
-  var pdfMerge = new PDFMerge(PDFs, output);
-
-  pdfMerge.merge(function(error, result) {
-    if (error != null) console.log(error);
-    else fs.writeFile(output, result);
-  });
-  
-  setTimeout(function () {
-    for (var i = 0; i < PDFs.length; i++) {
-    var child = exec('rm ./' + PDFs[i], function(err) {
-      if (err) console.log(err);
+function ejs2html(path, information, templateFile) {
+    var data = fs.readFileSync(path, 'utf8');
+    Q.when(data, function () { 
+      var ejs_string = data,
+          template = ejs.compile(ejs_string),
+          html = template(information);
+          // saves html file to htmlOutput directory
+      var appended = fs.appendFileSync(curpath + templateFile, html);  
+      Q.when(appended, function () {
+        return "appended";
+      });
     });
-  }}, 1000);
+  };
 
+function appendTitle(path, name, outputFile) { 
+  ejs2html(curpath + path, {title: name}, outputFile);
+}; 
+
+function appendBody(data, outputFile) {
+    for (var i = 0; i < data.length; i++) {
+    var orightml;
+    if ("type" in data[i]) {
+      if (data[i].type === "pie") { orightml = 'usageReportPieTemplate.ejs';}
+      else if (data[i].type === "table") { orightml = 'logsTableTemplate.ejs';}
+      else if (data[i].type === "timeSeries") { orightml ='timeSeriesTemplate.ejs';}
+      else if (data[i].type === "lineGraph") { orightml = 'xyGraphTemplate.ejs';}
+      else orightml = data[i].type;
+    } else {
+      console.log("ERROR: 'type' undefined");
+      return;
+    }
+      ejs2html(curpath+orightml, { raw: JSON.stringify(data[i]) }, outputFile); 
+    }
+}; 
+
+function createPDF(htmlToRender, output, pageNumbers, timeStamp) {
+  phantom.create(function(ph){
+    ph.createPage(function(page) {
+      page.viewportSize = { width: 1920, height: 1920 };
+
+        if (pageNumbers) {
+          var headerObj = 
+          { header: { height: '1cm',
+            contents: ph.callback(function(pageNum, numPages) {
+              return "<h1><font size='1'><span style='float:right'>" + pageNum + "</span></h1>";
+            })
+          }}
+        }
+        if (timeStamp) {
+          if (headerObj === undefined) {
+            var headerObj = {
+              header: { 
+              height: '1cm',
+              contents: ph.callback(function() {
+                return "<h1><font size='1'><span style='float:left'>" + new Date() + "</span></h1>";
+              })
+            }}
+        } else {
+          headerObj.header.contents = ph.callback(function(pageNum, numPages) {
+            return "<h1><font size='1'><span style='float:right'>" + pageNum + "</span>" + 
+              "<span style='float:left'>" + new Date() + "</span></h1>";
+          })
+        }
+      }
+    
+      if (pageNumbers || timeStamp) { 
+        page.set('paperSize', {format: 'Letter', 
+        orientation: 'portrait', margin:'1cm', header: headerObj.header});
+      } else {
+        page.set('paperSize', {format: 'Letter', orientation: 'portrait', margin:'1cm'});
+      }
+
+      page.set('zoomFactor', 1)
+      page.open(htmlToRender, function(status) {
+        if (status === "success") {
+          console.log("Page Opened");
+          page.render(output,function(){
+            console.log('Page Rendered');
+          });
+        } else {
+          console.log("ERROR: Not a valid template");
+        }
+        ph.exit();
+      });
+    });
+  });
 };
 
 exports.output = function(out, data, options) {
-	// options: timestamp, header with page numbers, own html template
-	// {timestamp: "on", pageNumbers: "on", type: ("table", "pie", "graph", or 'provide a template, no path')}
-	
-  if (out === "merge") {
-    out = "output/" + PDFs.length + ".pdf"
-    PDFs.push(out);
-  }
+
+  if (options.title === undefined) options.title = "";
+    
+    var file = options.template;
+    if (fs.existsSync(curpath + file)) {
+      var child = exec('rm ' + curpath + file, function(err) {
+        if (err) console.log(err);
+      });
+    }
   
-  var path = require('path');
-  var ejs = require('ejs');
-  var phantom = require('phantom');
-  var fs = require('fs');
+  appendTitle("title.ejs", options.title, file);
+  appendBody(data, file);
+  
+  var htmlRendered = curpath + 'htmlOutput/template.html';
+  createPDF(htmlRendered, out, options.pageNumbers, options.timestamp);
 
-	if (options === undefined) options = {}; // if options undefined, default options
-
-  var curpath = path.join(__dirname, 'templates/'); 
-
-	var orightml;
-	if ("type" in options) {
-		if (options.type === "pie") { orightml = 'usageReportPieTemplate.ejs';}
-		else if (options.type === "table") { orightml = 'logsTableTemplate.ejs';}
-		else if (options.type === "timeSeries") { orightml ='timeSeriesTemplate.ejs';}
-    else if (options.type === "lineGraph") { orightml = 'xyGraphTemplate.ejs';}
- 		else orightml = options.type;
- 	} else {
- 		console.log("ERROR: 'type' undefined");
- 		return;
- 	}
-
-// generates html from ejs template
-function ejs2html(path, information) {
-    fs.readFile(path, 'utf8', function (err, data) {
-        if (err) { console.log(err); return false; }
-        var ejs_string = data,
-            template = ejs.compile(ejs_string),
-            html = template(information);
-            // saves html file to htmlOutput directory
-        fs.writeFile(curpath + 'htmlOutput/' + orightml + '.html', html, function(err) { 
-            if(err) { console.log(err); return false }
-            return true;
-        });  
-    });
-}
-
-// call ejs2html to generate PDF
-if (options.title === undefined) options.title = "";
-ejs2html(curpath+orightml,
-  { pagename: options.title,
-    raw: JSON.stringify(data)
-  });
-
-var htmlRendered = curpath + 'htmlOutput/' + orightml + ".html"; // path to rendered file
-
-  phantom.create(function(ph){
-  ph.createPage(function(page) {
-    page.viewportSize = { width: 1920, height: 1920 };
-
-    if (options.pageNumbers === "on") {
-    var headerObj = 
-    	{ header: { height: '1cm',
-    				contents: ph.callback(function(pageNum, numPages) {
-      				return "<h1><font size='1'><span style='float:right'>" + pageNum + "</span></h1>";
-   				 })
-    	}}
-  	}
-  	if (options.timestamp === "on") {
-  		if (headerObj === undefined) {
-  			var headerObj = {
-  			header: { 
-  				height: '1cm',
-  				contents: ph.callback(function() {
-  					return "<h1><font size='1'><span style='float:left'>" + new Date() + "</span></h1>";
-  				})
-  			}}
-  		} else {
-  			headerObj.header.contents = ph.callback(function(pageNum, numPages) {
-      				return "<h1><font size='1'><span style='float:right'>" + pageNum + "</span>" + 
-      				"<span style='float:left'>" + new Date() + "</span></h1>";
-   				 })
-  		}
-  	}
-  	if (options.pageNumbers === "on" || 
-  		options.timestamp === "on") {	
-		page.set('paperSize', {format: 'Letter', 
-		orientation: 'portrait', margin:'1cm', header: headerObj.header});
-	} else {
-		page.set('paperSize', {format: 'Letter', orientation: 'portrait', margin:'1cm'});
-	}
-    page.set('zoomFactor', 1)
-    page.open(htmlRendered, function(status) {
-    	if (status === "success") {
-    		console.log("Page Opened");
-    		page.render(out,function(){
-        		console.log('Page Rendered');
-      		});
-    	} else {
-      	console.log("ERROR: Not a valid template");
-      	}
-      	ph.exit();
-    });
-  });
-});
 };
-
-
--- public/test.js
+++ public/test.js
@@ -1,76 +1,50 @@
 var pdf = require('./htmlToPdf.js');
-//var arrayPdf = require('./htmlToPdfArray.js');
 
-pdf.output("output/pie.pdf", 
-	[{label:"Running", data:20},
-      {label:"Idle", data:60},
-      {label:"Stalled", data:20}], 
-     {title: "Usage Report", type: "pie", pageNumbers: "on", timestamp: "on"});
+pdf.output("output/report.pdf", [
+    
+      {type: "pie", 
+       title: "Usage Report",
+       data: [{label:"Running", data:20},
+              {label:"Idle", data:60},
+              {label:"Stalled", data:20}]},
+     
+     {type: "timeSeries", 
+      title: "TimeSeries", 
+      xtitle: "date", 
+      ytitle: "value", // axis lables for graphs, use "" for blank
+      data: [{value:3, date:new Date(2000, 1, 1, 5, 00).getTime()},
+             {value:8, date:new Date(2000, 1, 1, 6, 30).getTime()},
+             {value:5, date:new Date(2000, 1, 1, 11, 15).getTime()},
+             {value:13, date:new Date(2000, 1, 1, 22, 00).getTime()},
+             {value:4, date:new Date(2000, 1, 2, 5, 00).getTime()}]},
+     
+     {type: "lineGraph", 
+      title: "XY Graph", 
+      xtitle: "THIS IS X", 
+      ytitle: "THIS IS Y",
+      data: [{x:1, y:3}, // axis lables for graphs, use "" for blank
+             {x:2, y:8},
+             {x:3, y:5},
+             {x:4, y:13},
+             {x:5, y:4}]},
+     
+     {type: "logsTableTemplate.ejs", 
+      title: "Event Log",
+      data: [{ Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
+             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
+             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
+             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
+             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
+             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
+             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
+             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
+             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
+             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
+             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
+             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"} ]}
+      
+       ],
 
-pdf.output("merge", 
-     [{xtitle: "date"}, {ytitle: "value"}, // axis lables for graphs, use "" for blank
-      [{value:3, date:new Date(2000, 1, 1, 5, 00).getTime()},
-      {value:8, date:new Date(2000, 1, 1, 6, 30).getTime()},
-      {value:5, date:new Date(2000, 1, 1, 11, 15).getTime()},
-      {value:13, date:new Date(2000, 1, 1, 22, 00).getTime()},
-      {value:4, date:new Date(2000, 1, 2, 5, 00).getTime()}]], 
-     {title: "Time v Value", type: "timeSeries", pageNumbers: "on", timestamp: "on"});
-
-
-pdf.output("merge", 
-	[{xtitle: "THIS IS X"},{ytitle: "THIS IS Y"},
-      [{x:1, y:3}, // axis lables for graphs, use "" for blank
-      {x:2, y:8},
-      {x:3, y:5},
-      {x:4, y:13},
-      {x:5, y:4}]], 
-     {title: "X-Y", type: "lineGraph", pageNumbers: "on", timestamp: "on"});
- 
-pdf.output("merge", 
-	[{ Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"} ], 
-     {title: "Event Log", type: "table", pageNumbers: "on", timestamp: "on"});
-
-setTimeout(function () { pdf.concat("output/report.pdf"); }, 3000);
-
-// arrayPdf.arrayOutput("output/arrayOutput.pdf", [
-//      [{label:"Running", data:20},
-//       {label:"Idle", data:60},
-//       {label:"Stalled", data:20}],
-//      [{xtitle: "date"}, {ytitle: "value"}, // axis lables for graphs, use "" for blank
-//       [{value:3, date:new Date(2000, 1, 1, 5, 00).getTime()},
-//       {value:8, date:new Date(2000, 1, 1, 6, 30).getTime()},
-//       {value:5, date:new Date(2000, 1, 1, 11, 15).getTime()},
-//       {value:13, date:new Date(2000, 1, 1, 22, 00).getTime()},
-//       {value:4, date:new Date(2000, 1, 2, 5, 00).getTime()}]],
-//      [{xtitle: "THIS IS X"},{ytitle: "THIS IS Y"},
-//       [{x:1, y:3}, // axis lables for graphs, use "" for blank
-//       {x:2, y:8},
-//       {x:3, y:5},
-//       {x:4, y:13},
-//       {x:5, y:4}]],
-//      [{ Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-//       { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-//       { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-//       { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-//       { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-//       { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-//       { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-//       { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-//       { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-//       { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-//       { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-//       { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"} ]],
-//       {title: "Usage Report", type: 'template.ejs', pageNumbers: 'on'});
+      {title: "Full Report", pageNumbers: true, timestamp: true, template: "htmlOutput/template.html"});
 
 
