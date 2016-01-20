f085f7600234da4c45658195a37381fbbf4c2c9f
Wendy Wei <wendywei@mit.edu>
2016-01-20 16:07:05 -0500

    cleanup

--- public/test.js
+++ public/test.js
@@ -3,22 +3,6 @@ var pdf = require('./htmlToPdf.js');
 pdf.output("output/customTest.pdf", 
      "/customTemplate.ejs",
      {
-     // title1: 'Logs Table',
-     // table:{
-     //      data: [{ Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-     //             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-     //             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-     //             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-     //             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-     //             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-     //             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-     //             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-     //             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-     //             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-     //             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
-     //             { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"}],
-     //      id: 'table'
-     // },
      pie: {
           id: 'pie',
           title: 'Usage Pie',
@@ -41,45 +25,3 @@ pdf.output("output/customTest.pdf",
      },
 
      {pageNumbers: "on", timestamp: "on"});
-
-// pdf.output("output/pie.pdf", 
-// 	[{label:"Running", data:20},
-//       {label:"Idle", data:60},
-//       {label:"Stalled", data:20}], 
-//      {title: "Usage Report", type: "pie", pageNumbers: "on", timestamp: "on"});
-
-// pdf.output("merge", 
-//      [{xtitle: "date"}, {ytitle: "value"}, // axis lables for graphs, use "" for blank
-//       [{value:3, date:new Date(2000, 1, 1, 5, 00).getTime()},
-//       {value:8, date:new Date(2000, 1, 1, 6, 30).getTime()},
-//       {value:5, date:new Date(2000, 1, 1, 11, 15).getTime()},
-//       {value:13, date:new Date(2000, 1, 1, 22, 00).getTime()},
-//       {value:4, date:new Date(2000, 1, 2, 5, 00).getTime()}]], 
-//      {title: "Time v Value", type: "timeSeries", pageNumbers: "on", timestamp: "on"});
-
-
-// pdf.output("merge", 
-// 	[{xtitle: "THIS IS X"},{ytitle: "THIS IS Y"},
-//       [{x:1, y:3}, // axis lables for graphs, use "" for blank
-//       {x:2, y:8},
-//       {x:3, y:5},
-//       {x:4, y:13},
-//       {x:5, y:4}]], 
-//      {title: "X-Y", type: "lineGraph", pageNumbers: "on", timestamp: "on"});
- 
-// pdf.output("merge", 
-// [{ Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
- //      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
- //      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
- //      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
- //      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
- //      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
- //      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
- //      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
- //      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
- //      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
- //      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"},
- //      { Timestamp:"12:00:00" , TSLink:"TS-Link0" , Username:"user-name0" , Event:"event-0"} ], 
-//      {title: "Event Log", type: "table", pageNumbers: "on", timestamp: "on"});
-
-// setTimeout(function () { pdf.concat("output/report.pdf"); }, 3000);
